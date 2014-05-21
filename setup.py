f0fd60506802444fb130f8ae6ab76df696368262
Peter <peter1010@users.noreply.github.com>
2014-05-21 21:49:31 +0100

    fix up ownership

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
 import os
+import pwd
+import subprocess
+
 from distutils.core import setup
 from distutils.command import install_data
 
 class my_install_data(install_data.install_data):
     def run(self):
         retVal = super().run()
-        for path in self.outfile:
+        for path in self.outfiles:
             if path.startswith("/var"):
-                os.chown()
+                o = pwd.getpwnam("nobody")
+                os.chown(path, o.pw_uid, o.pw_gid)
         print( self.outfiles)
+        subprocess.call(["systemctl","enable","track_my_ip.timer"])
+        subprocess.call(["systemctl","start","track_my_ip.timer"])
 
 setup(name='track_my_ip',
       version='1.0',
@@ -21,8 +27,10 @@ setup(name='track_my_ip',
       license='GPL',
       packages=['track_my_ip'],
       package_dir={'':'src'},
-      data_files=[('/usr/lib/systemd/system',('track_my_ip.timer','track_my_ip.service')),
-                  ('/var/cache/track_my_ip',[])],
+      data_files=[\
+            ('/usr/lib/systemd/system',
+                ('track_my_ip.timer','track_my_ip.service')),
+            ('/var/cache/track_my_ip',[])],
       cmdclass = {'install_data': my_install_data}
       )
 
