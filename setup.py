50383e0c63d8778646787c1f8ca0ae8a64db4a4e
Peter <peter1010@users.noreply.github.com>
2014-05-23 20:56:42 +0100

    Moved service start and stop to python service file

--- setup.py
+++ setup.py
@@ -2,38 +2,16 @@
 
 import os
 import pwd
-import subprocess
-
 from distutils.core import setup
 from distutils.command import install_data, install
-
-
-def start_service():
-    subprocess.call(["systemctl","enable","track_my_ip.timer"])
-    subprocess.call(["systemctl","start","track_my_ip.timer"])
-
-
-def stop_service():
-    subprocess.call(["systemctl","stop","track_my_ip.timer"])
-    subprocess.call(["systemctl","disable","track_my_ip.timer"])
-
-
-class my_install_data(install_data.install_data):
-    def run(self):
-        retVal = super().run()
-        for path in self.outfiles:
-            if path.startswith("/var"):
-                o = pwd.getpwnam("nobody")
-                os.chown(path, o.pw_uid, o.pw_gid)
-        print( self.outfiles)
-        return retVal
 
 
 class my_install(install.install):
     def run(self):
         retVal = super().run()
         if not os.getenv("DONT_START"):
-            start_service()
+            from src import service
+            service.start_service()
         return retVal;
 
 
@@ -44,12 +22,10 @@ setup(name='track_my_ip',
       author='Peter1010',
       author_email='peter1010@localnet',
       license='GPL',
+      package_dir={'track_my_ip':'src'},
       packages=['track_my_ip'],
-      package_dir={'':'src'},
       data_files=[\
             ('/usr/lib/systemd/system',
-                ('track_my_ip.timer','track_my_ip.service')),
-            ('/var/cache/track_my_ip',[])],
-      cmdclass = {'install_data': my_install_data,
-                  'install' : my_install}
+                ('track_my_ip.timer','track_my_ip.service'))],
+      cmdclass = {'install' : my_install}
       )
--- src/service.py
+++ src/service.py
@@ -1,18 +1,21 @@
+import sys
+import grp
+import pwd
+import subprocess
+import os
 
 def create_group(group):
-    import grp, subprocess
     try:
         info = grp.getgrnam(group)
         gid = info.gr_gid
     except KeyError:
-        subprocess.call(["groupadd","-K", "GID_MIN=250", group])
+        subprocess.call(["groupadd","-r", group])
         info = grp.getgrnam(group)
         gid = info.gr_gid
     return gid
 
 
 def remove_group(group):
-    import grp, subprocess
     try:
         info = grp.getgrnam(group)
         subprocess.call(["groupdel", group])
@@ -22,29 +25,37 @@ def remove_group(group):
 
 
 def create_user(user, gid):
-    import pwd, subprocess
     try:
         info = pwd.getpwnam(user)
         uid = info.pw_uid
     except KeyError:
-        subprocess.check(["useradd","-K","UID_MIN=250", "-g", gid, "-s", "/bin/false". user])
+        subprocess.call(["useradd","-r", "-g", str(gid), 
+                "-s", "/bin/false", user])
         info = pwd.getpwnam(user)
         uid = info.pw_uid
     return uid
 
 
 def remove_user(user):
-    import pwd, subprocess
     try:
         info = pwd.getpwnam(user)
-        subprocess.check(["userdel". user])
+        subprocess.call(["userdel", user])
     except KeyError:
         pass
 
 
+def make_dir(path, uid, gid):
+    if not os.path.exists(path):
+        os.mkdir(path)
+    os.chown(path, uid, gid)
+    for f in os.listdir(path):
+        os.chown(os.path.join(path,f), uid, gid)
+
+
 def start_service():
-    create_group("track_my_ip")
-    create_user("track_my_ip")
+    gid = create_group("track_my_ip")
+    uid = create_user("track_my_ip", gid)
+    make_dir(os.path.join("/var","cache", "track_my_ip"), uid, gid)
     subprocess.call(["systemctl","enable","track_my_ip.timer"])
     subprocess.call(["systemctl","start","track_my_ip.timer"])
 
@@ -55,4 +66,8 @@ def stop_service():
     remove_user("track_my_ip")
     remove_group("track_my_ip")
 
-
+if __name__ == "__main__":
+    if sys.argv[1] == "start":
+        start_service()
+    else:
+        stop_service()
