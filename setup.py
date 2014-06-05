55f5dd7f99d2f22a98178aecbd58c9a68079c5b4
Peter <peter1010@users.noreply.github.com>
2014-06-05 21:05:05 +0100

    Fixed errors found when run through pyflakes

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 
 import os
-import pwd
 from distutils.core import setup
-from distutils.command import install_data, install
+from distutils.command import install
 
 
 class my_install(install.install):
@@ -13,20 +12,22 @@ class my_install(install.install):
             if not os.getenv("DONT_START"):
                 from src import service
                 service.start_service()
-        return retVal;
+        return retVal
 
 
-setup(name='track_my_ip',
-      version='1.0',
-      description="Keep track of current public IP address",
-      url='https://github.com/peter1010/track_my_ip',
-      author='Peter1010',
-      author_email='peter1010@localnet',
-      license='GPL',
-      package_dir={'track_my_ip':'src'},
-      packages=['track_my_ip'],
-      data_files=[\
-            ('/usr/lib/systemd/system',
-                ('track_my_ip.timer','track_my_ip.service'))],
-      cmdclass = {'install' : my_install}
-      )
+setup(
+    name='track_my_ip',
+    version='1.0',
+    description="Keep track of current public IP address",
+    url='https://github.com/peter1010/track_my_ip',
+    author='Peter1010',
+    author_email='peter1010@localnet',
+    license='GPL',
+    package_dir={'track_my_ip': 'src'},
+    packages=['track_my_ip'],
+    data_files=[
+        ('/usr/lib/systemd/system',
+         ('track_my_ip.timer', 'track_my_ip.service'))
+    ],
+    cmdclass={'install': my_install}
+)
